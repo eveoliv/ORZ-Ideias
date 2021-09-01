b3429bb9fcf6aef96d86e025ee05bcb3ce3c0085
Everton Oliveira <everton_ace@hotmail.com>
2021-08-31 22:49:40 -0300

    Incluindo inicializacao de tbls sqlserver

---
+++ Orizon.MapaMotorRegras.Api/Context/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Orizon.MapaMotorRegras.Api.Entidades;
+
+namespace Orizon.MapaMotorRegras.Api.Context
+{
+    public class DbInitializer
+    {
+        public static void Initializer(MapaMotorContext context)
+        {
+            if (context.Regras.Any())
+                return;
+
+            var insertRegras = new Regra[]
+            {
+                new Regra { Opereadora = 48, Mapa = "{'Rules': [ 1238,4556,4789,1011,10128,10135 ]}"},
+                new Regra { Opereadora = 25, Mapa = "{'Rules': [ 1123,1456,1789,1111,11012,11013 ]}"},
+                new Regra { Opereadora = 60, Mapa = "{'Rules': [ 1235,4565,7895,1015,10125,10135 ]}"},
+                new Regra { Opereadora = 32, Mapa = "{'Rules': [ 1239,9456,8789,1011,81012,21013 ]}"}
+            };
+
+            foreach (var item in insertRegras)            
+                context.Regras.Add(item);
+
+            context.SaveChanges();            
+        }
+    }
+}
--- Orizon.MapaMotorRegras.Api/Program.cs
+++ Orizon.MapaMotorRegras.Api/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Orizon.MapaMotorRegras.Api.Context;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Orizon.MapaMotorRegras.Api
 {
@@ -7,7 +11,27 @@ namespace Orizon.MapaMotorRegras.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            //Desenvolvimento iniciar base de dados
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MapaMotorContext>();
+                    DbInitializer.Initializer(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Erro ao popular a base de dados");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
